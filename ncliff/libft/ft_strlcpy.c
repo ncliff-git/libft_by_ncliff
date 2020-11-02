4086bd3b61cbf9e20a59bc1c8f9cb859bb8414e4
Nikia Cliff <ncliff@va-h1.kzn.21-school.ru>
2020-11-02 21:41:08 +0300

    02.11.2020 fail)

--- ncliff/libft/ft_strlcpy.c
+++ ncliff/libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ncliff <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 15:28:54 by ncliff            #+#    #+#             */
-/*   Updated: 2020/10/30 15:28:56 by ncliff           ###   ########.fr       */
+/*   Updated: 2020/11/02 21:35:41 by ncliff           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	int		pntd;
 
 	pntd = 0;
-	if (!dst || !src || dstsize <= 0)
+	if (!dst || !src)
 		return (sizeof(0));
+	if (dstsize <= 0)
+		return (ft_strlen(src));
 	sizedst = ft_strlen(src);
 	while (src[pntd] != '\0' && dstsize - 1 > 0)
 	{
